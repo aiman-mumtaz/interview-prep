cc1ea7ff50eceb5ab0a68c257bc8a73c288f5d73
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-20 00:56:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (93.45%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
+    // double calc(double x,double pw, long n){
+    //     if(n==0){
+    //         return pw;
+    //     }
+    //     pw = x*pw;
+    //     return calc(x,pw,n-1);
+    // }
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n==0){
+            return 1;
+        }
+        if(n<0){
+            if(n==INT_MIN){
+                return (1/x)*myPow(1/x,-(n+1));
+            }
+            return myPow(1/x,-n);
+        }
+        return (n%2 == 0 ? myPow(x*x,n/2):x*myPow(x*x,n/2));
     }
 };
