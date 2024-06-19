dc410c01f4757e46e355ed6973879ab5b572a8f0
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-20 03:03:04 +0530

    Time: 16 ms (66.88%), Space: 27 MB (58.9%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
-    void recc(int i,vector<char>& s){
-        if(i>=s.size()/2){
-            return;
-        }
-        swap(s[i],s[s.size()-i-1]);
-        recc(i+1,s);
-    } 
     void reverseString(vector<char>& s) {
-        // int i=0;
-        // int j=s.size()-1;
-        // while(i<j){
-        //     swap(s[i],s[j]);
-        //     i++;
-        //     j--;
-        // }
-        recc(0,s);
+        int i=0;
+        while(i<s.size()/2){
+            swap(s[i],s[s.size()-i-1]);
+            i++;
+        }
     }
 };
