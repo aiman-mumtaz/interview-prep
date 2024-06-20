434d3b98b1f073073daf01d0d0241ef0ea20daad
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-21 03:18:28 +0530

    Time: 33 ms (39.8%), Space: 25.6 MB (73.43%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    static bool cmp(const pair<string,int>& b,const pair<string,int>& a){
+        return a.second<b.second;
+    }
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<string,int>> vec;
+        for(int i=0;i<names.size();i++){
+            vec.push_back(make_pair(names[i],heights[i]));
+        }
+        sort(vec.begin(),vec.end(),cmp);
+        for(int i=0;i<vec.size();i++){
+            names[i]=vec[i].first;
+        }
+        return names;
+    }
+};
