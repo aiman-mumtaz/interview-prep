958a7a1e45f8094adbf091288d399490c132f1b4
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-21 03:22:59 +0530

    Time: 24 ms (81.88%), Space: 25.6 MB (74.37%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.cpp
+++ 2502-sort-the-people/2502-sort-the-people.cpp
@@ -5,6 +5,7 @@ public:
     }
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
         vector<pair<string,int>> vec;
+        // vector<string> vec(names.size());
         for(int i=0;i<names.size();i++){
             vec.push_back(make_pair(names[i],heights[i]));
         }
