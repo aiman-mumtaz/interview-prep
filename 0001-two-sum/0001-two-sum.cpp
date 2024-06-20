0f684d5fd0b20aeb77234000f1366d0c3c2b2684
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-21 00:16:09 +0530

    Time: 12 ms (59.59%), Space: 13.9 MB (40.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& a, int target) {
         unordered_map<int,int> mpp;
-        vector<int> v(2,0);
+        // vector<int> v(2,0);
         for(int i=0;i<a.size();i++){
             if(mpp.find(target - a[i]) != mpp.end()){
-                // return {i,mpp.get(target-a[i]);}
-                v[0]=i;
-                v[1]=mpp[target-a[i]];
-                return v;
+                return {i,mpp[target-a[i]]};
+                // v[0]=i;
+                // v[1]=mpp[target-a[i]];
+                // return v;
             }else{
                 mpp[a[i]] = i;
             }
         }
-        return v;
+        return {};
     }
 };
