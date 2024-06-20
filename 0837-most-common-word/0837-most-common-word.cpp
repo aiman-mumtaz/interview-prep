ba7daea53662edace3a768a55f015bd9fa85ba06
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-21 02:10:35 +0530

    Time: 8 ms (15.14%), Space: 10 MB (78.76%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.cpp
+++ 0837-most-common-word/0837-most-common-word.cpp
@@ -7,25 +7,21 @@ public:
             }
             ch=tolower(ch);
         }
-        // cout<<s<<endl;
         map<string,int> mpp;
-        // vector<string> str;
+        
         string p;
         istringstream iss(s);
         while ( iss>>p ) {
             if(find(banned.begin(),banned.end(),p.c_str()) == banned.end())
                 mpp[p.c_str()]++;
         }
-        // sort(mpp.begin(),mpp.end(),comp);
         pair<string,int> px;
         for(auto x:mpp){
-            // cout<<x.first<<" "<<x.second<<endl;
             if(px.second < x.second){
                 px.first = x.first;
                 px.second = x.second;
             }
         }
-        // cout<<px.first<<" "<<px.second;
         return px.first;
     }
 };
