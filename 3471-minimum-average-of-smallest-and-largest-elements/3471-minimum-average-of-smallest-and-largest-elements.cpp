869b4d7d5df6f0344dd4f2afbfb24df09fbeb807
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-23 19:30:02 +0530

    Time: 4 ms (57.14%), Space: 25.3 MB (28.57%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
@@ -4,21 +4,14 @@ public:
         sort(a.begin(),a.end());
         vector<double> avg;
         for(int i=0;i<a.size()/2;i++){
-            double minn = a[i];
-            double maxx=a[a.size()-i-1];
-            avg.push_back((minn+maxx)/2);
+            avg.push_back(double(a[i]+a[a.size()-i-1])/2);
         }
-        // for(auto x:avg){
-        //     cout<<x<<" ";
-        // }
         double min = INT_MAX;
         for(int i=0;i<avg.size();i++){
             if(avg[i]<min){
                 min =avg[i];
             }
         }
-        // sort(avg.begin(),avg.end());
-        // return avg[0];
         return min;
     }
 };
