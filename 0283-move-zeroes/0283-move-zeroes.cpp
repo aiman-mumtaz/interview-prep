3cdef0c12579df087236800b7adb13d560309f7d
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-24 01:23:52 +0530

    Time: 25 ms (12.05%), Space: 21.6 MB (73.77%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& a) {
-        // int s=0,e=a.size()-1;
-        // while(s<e){
-        //     if(a[s] == 0){
-        //         swap(a[s],a[e]);
-        //         e--;
-        //     }
-        //     s++;
-        // }
-        if(a.size()==1 && a[0] == 0){
-            return;
-        }
         int cnt=0;
-        // int indexes[a.size()-1];
         for(int i=0;i<a.size();i++){
             if(a[i]==0){
                 a.erase(a.begin()+i);
@@ -21,11 +9,6 @@ public:
                 i--;
             }
         }
-        // if(a[0] == 0){
-        //     a.erase(a.begin());
-        //     cnt++;
-        // }
-        // cout<<a.size();
         while(cnt--){
             a.push_back(0);
         }
