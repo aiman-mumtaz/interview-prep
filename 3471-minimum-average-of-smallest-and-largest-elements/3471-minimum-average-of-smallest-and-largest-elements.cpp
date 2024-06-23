800653dea18150f0ba8ee7098fa7b21da7f9ebb9
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-23 19:31:33 +0530

    Time: 0 ms (100%), Space: 25 MB (28.57%) - LeetHub

--- 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.cpp
@@ -2,14 +2,10 @@ class Solution {
 public:
     double minimumAverage(vector<int>& a) {
         sort(a.begin(),a.end());
-        vector<double> avg;
-        for(int i=0;i<a.size()/2;i++){
-            avg.push_back(double(a[i]+a[a.size()-i-1])/2);
-        }
         double min = INT_MAX;
-        for(int i=0;i<avg.size();i++){
-            if(avg[i]<min){
-                min =avg[i];
+        for(int i=0;i<a.size()/2;i++){
+            if(double(a[i]+a[a.size()-i-1])/2 < min ){
+                min=double(a[i]+a[a.size()-i-1])/2;
             }
         }
         return min;
