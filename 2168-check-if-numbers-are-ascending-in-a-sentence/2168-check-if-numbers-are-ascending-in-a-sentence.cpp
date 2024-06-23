64925053ccb0f45d53d21d39caa58383d0296ec3
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-24 02:15:28 +0530

    Time: 0 ms (100%), Space: 8 MB (12.56%) - LeetHub

--- 2168-check-if-numbers-are-ascending-in-a-sentence/2168-check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/2168-check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool areNumbersAscending(string s) {
-        vector<string> v;
+        vector<int> v;
         stringstream ss(s);
         string p;
         while(ss >> p){
-            v.push_back(p);
+            if(p[0] >= '0' && p[0]<='9')
+            v.push_back(stoi(p));
         }
         int maxx = INT_MIN;
-        
         for(auto x: v){
-            if(x[0] >= '0' && x[0] <= '9'){
-                if(stoi(x) <= maxx){
-                    return false;
-                }
-                maxx = stoi(x);
+            if(x<=maxx){
+                return false;
             }
+            maxx=x;
         }
         return true;
     }
