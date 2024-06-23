35223ba5755ef8eb94b6264a243482b9ea17a7d4
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-24 02:44:36 +0530

    Time: 24 ms (77.18%), Space: 29.9 MB (23.46%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -3,10 +3,10 @@ public:
     int search(vector<int>& a, int target) {
         int s=0,e=a.size()-1;
         while(s<=e){
-            // int mid = (s+e)/2;
+            int mid = (s+e)/2;
             if(a[(s+e)/2] == target){
-                return (s+e)/2;
-            }else if(a[(s+e)/2] > target){
+                return mid;
+            }else if(a[mid] > target){
                 e--;
             }else{
                 s++;
