aaaabf9edc785d150257c64149e86fa9aa175486
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-25 03:01:38 +0530

    Time: 3 ms (71.29%), Space: 12.1 MB (46.01%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& a, int k) {
-        int index= 0;
+
         int s=0,e=a.size()-1;
         while(s<=e){
             int mid = (s+e)/2;
@@ -9,13 +9,10 @@ public:
                 return mid;
             }else if(a[mid]>k){
                 e=mid-1;
-                index=mid;
             }else{
                 s=mid+1;
-                index=s;
             }
-            cout<<index<<" ";
         }
-        return index;
+        return s;
     }
 };
