c5c0f42fdd5386bcc6b0837cdfb2232d8ffa6b40
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-06-27 01:24:17 +0530

    Time: 21 ms (55.53%), Space: 28.6 MB (5.8%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<int>& a, int p) {
+        vector<int> ans;
+        
+        int j=0;
+        int x = p%a.size();
+        if(a.size()>x) {
+            j = a.size()-x;
+        }else if(a.size() <x){
+            j = a.size()-1;
+        }
+        for(int k=j;k<a.size();k++){
+            ans.push_back(a[k]);
+        }
+        if(j>0) {
+            for(int i=0;i<j;i++){
+                ans.push_back(a[i]);
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            a[i] = ans[i];
+        }
+    }
+};
