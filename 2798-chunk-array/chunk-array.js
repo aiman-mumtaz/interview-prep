405adfe83e893338a7a234bb5f21367ba5424dcb
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-07-05 03:21:43 +0530

    Time: 51 ms (80.03%) | Memory: 50.8 MB (95.44%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const ans = [];
+    for(let i=0;i<arr.length;i+=size){
+        ans.push(arr.slice(i,i+size));
+    }
+    return ans;
+};
