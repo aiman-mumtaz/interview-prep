680ff0aebc082a8813947b967521112c9dbbc230
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-07-07 00:56:28 +0530

    Time: 2 ms (54.14%) | Memory: 7.3 MB (30.58%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.cpp
+++ 2645-pass-the-pillow/pass-the-pillow.cpp
@@ -4,9 +4,11 @@ public:
         if(time >= n){
             int x = time%(n-1);
             int y = time/(n-1);
+            cout<<x<<" "<<y;
             if(y%2 == 0)
                 return x+1;
             else{
+                // return time - x*(n-1);
                 return n-x;
             }
         }
