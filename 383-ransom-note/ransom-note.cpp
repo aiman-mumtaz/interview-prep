5083ed4c7464d52fb201fa72c6e4ba18c4503da6
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2024-09-11 10:35:01 +0530

    Time: 7 ms (97.04%) | Memory: 10.2 MB (92.25%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int arr[26] = {0};
+        int arr1[26] = {0};
+        for(char x: ransomNote){
+            arr[x-'a']++;
+        }
+        for(char x: magazine){
+            arr1[x-'a']++;
+        }
+
+        for(int i=0;i<26;i++){
+            if(arr[i] > arr1[i]){
+                return false;
+            }
+            // cout<<x<<"\n";
+            // cout<<x.first<<" "<<x.second<<endl;
+        }
+        return true;
+    }
+};
