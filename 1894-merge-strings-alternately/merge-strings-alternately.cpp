fd7b5904ce3cbb66a68ef243221bef640bfcf190
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-04 22:50:35 +0530

    Time: 2 ms (43.28%) | Memory: 9.6 MB (6.09%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -4,14 +4,10 @@ public:
         int i=0,j=0;
         string ans="";
         while(i<word1.length() && j<word2.length()){
-            cout<<word1[i]<<" "<<word2[j]<<"\n";
             ans = ans+ word1[i]+word2[j];
             i++;
             j++;
         }
-        // cout<<i<<" "<<j<<"\n";
-        // cout<<word1.length()<<" "<<word2.length();
-        // return "";
         while(i<word1.length()){
             ans += word1[i];
             i++;
