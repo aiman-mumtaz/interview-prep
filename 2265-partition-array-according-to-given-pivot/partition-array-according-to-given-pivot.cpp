8106d8a7ef196bbfc1c0f0bba3982eadbad283b6
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-13 00:28:16 +0530

    Time: 2 ms (88.57%) | Memory: 127.8 MB (69.71%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& a, int pivot) {
-        // vector<int>lesser,greater,equal;
-        // for(int i=0;i<a.size();i++){
-        //     if(a[i] < pivot){
-        //         lesser.push_back(a[i]);
-        //     }else if(a[i] > pivot){
-        //         greater.push_back(a[i]);
-        //     }else{
-        //         equal.push_back(a[i]);
-        //     }
-        // }
-        // lesser.insert(lesser.end(),equal.begin(),equal.end());
-        // lesser.insert(lesser.end(),greater.begin(),greater.end());
-        // return lesser;
         vector<int> ans(a.size(),0);
         int sI=0,gI = a.size(),pI=0;
         for(int i=0;i<a.size();i++){
