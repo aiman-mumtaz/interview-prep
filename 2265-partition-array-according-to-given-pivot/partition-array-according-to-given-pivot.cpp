0717f5fff5012cbbcad8dcf687a101f650ec2494
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-13 00:27:58 +0530

    Time: 11 ms (27.77%) | Memory: 127.5 MB (99.60%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& a, int pivot) {
-        vector<int>lesser,greater,equal;
+        // vector<int>lesser,greater,equal;
+        // for(int i=0;i<a.size();i++){
+        //     if(a[i] < pivot){
+        //         lesser.push_back(a[i]);
+        //     }else if(a[i] > pivot){
+        //         greater.push_back(a[i]);
+        //     }else{
+        //         equal.push_back(a[i]);
+        //     }
+        // }
+        // lesser.insert(lesser.end(),equal.begin(),equal.end());
+        // lesser.insert(lesser.end(),greater.begin(),greater.end());
+        // return lesser;
+        vector<int> ans(a.size(),0);
+        int sI=0,gI = a.size(),pI=0;
         for(int i=0;i<a.size();i++){
-            if(a[i] < pivot){
-                lesser.push_back(a[i]);
-            }else if(a[i] > pivot){
-                greater.push_back(a[i]);
-            }else{
-                equal.push_back(a[i]);
+            if(a[i] > pivot){
+                gI--;
+            }
+            if(a[i] == pivot){
+                pI++;
             }
         }
-        lesser.insert(lesser.end(),equal.begin(),equal.end());
-        lesser.insert(lesser.end(),greater.begin(),greater.end());
-        return lesser;
+        int index = 0;
+        pI=gI-pI;
+        while(sI<a.size()){
+            if(a[sI]<pivot){
+                ans[index++] = a[sI++];
+            }else if(a[sI] > pivot){
+                ans[gI++] = a[sI++];
+            }else{
+                ans[pI++] = a[sI++];
+            }
+        }
+        return ans;
     }
 };
