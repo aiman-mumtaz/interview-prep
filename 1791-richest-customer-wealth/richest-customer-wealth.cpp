fda5b202b7ee1af542c84e3c3d0e8b8a85664fe6
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-13 02:15:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (58.24%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.cpp
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -3,7 +3,7 @@ public:
     int maximumWealth(vector<vector<int>>& a) {
         int ans = 0;
         for(int i=0;i<a.size();i++){
-            int tmp =0;
+            int tmp=0;
             for(int j=0;j<a[0].size();j++){
                 tmp += a[i][j];
             }
