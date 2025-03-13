0081574f6ccae9f2a754eed9439c9a685b96a2ca
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-13 23:51:55 +0530

    Time: 2 ms (42.31%) | Memory: 80.4 MB (74.89%) - LeetSync

--- 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -4,7 +4,6 @@ public:
         vector<int> ans(pref.size(),0);
         ans[0] = pref[0];
         for(int i=1;i<pref.size();i++){
-            cout<<(pref[i-1] ^ pref[i])<<" ";
             ans[i] = pref[i-1] ^ pref[i];
         }
         return ans;
