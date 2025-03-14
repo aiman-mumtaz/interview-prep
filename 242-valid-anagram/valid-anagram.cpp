ecb083a3927113b4670b1bf97479729da8ffcc6f
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-15 02:37:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (48.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
+        if(s==t){
+            return true;
+        }
+        if(s.length() != t.length()){
+            return false;
+        }
+        vector<int> sfreq(26,0);
+        vector<int> tfreq(26,0);
+        int i=0,j=0;
+        for(int i=0;i<s.length();i++){
+            sfreq[s[i]-'a']++;
+            tfreq[t[i]-'a']++;
+        }
 
-        return s==t;
+        return sfreq==tfreq;
     }
 };
