3154530a1eb623ec60769f992bcfe6761e58e6bb
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-17 01:27:19 +0530

    Time: 4 ms (50.09%) | Memory: 10.7 MB (20.32%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    unordered_map<string,string> mpp;
+    string characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+public:
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        string shortUrl = "https://tinyurl.com/";
+        if(mpp.find(longUrl) != mpp.end()){
+            return mpp[longUrl];
+        }else{
+            int len = 7;
+            for (int i = 0; i < len; ++i) {
+                shortUrl += characters[rand() % (sizeof(characters) - 1)];
+            }
+            // cout<<longUrl<<" "<<shortUrl<<" ";
+            mpp.insert({longUrl,shortUrl});
+        }
+        return shortUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        for(auto x: mpp){
+            if(x.second == shortUrl){
+                return x.first;
+            }
+        }
+        return "";
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
