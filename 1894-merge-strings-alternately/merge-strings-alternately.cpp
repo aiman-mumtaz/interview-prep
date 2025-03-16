13197bdb6d73fea0e6e45eb9b6b1a6580b1c0d2a
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-17 03:56:47 +0530

    Time: 3 ms (40.10%) | Memory: 10.1 MB (5.03%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -3,18 +3,18 @@ public:
     string mergeAlternately(string a, string b) {
         int i=0,j=0;
         string ans = "";
-        while(i<a.length() && j<b.length()){
-            ans = ans + a[i] + b[j];
-            i++;
-            j++;
-        }
-        while(i<a.length()){
-            ans = ans + a[i];
-            i++;
-        }
-        while(j<b.length()){
-            ans = ans + b[j];
-            j++;
+        while(i<a.length() || j<b.length()){
+            if(i<a.length() && j<b.length()){
+                ans = ans + a[i] + b[j];
+                i++;
+                j++;
+            }else if(i<a.length()){
+                ans = ans + a[i];
+                i++;
+            }else if(j<b.length()){
+                ans = ans + b[j];
+                j++;
+            }
         }
         return ans;
     }
