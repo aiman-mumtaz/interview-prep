58ec291676207f6767e173589732114f3214e1c6
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-17 02:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (6.13%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& a) {
+        vector<int> ans;
+        int cnt=0;
+        for(int i=0;i<a.size();i++){
+            if(a[i] == 0){
+                cnt++;
+            }else{
+                ans.push_back(a[i]);
+            }
+        }
+        while(cnt--){
+            ans.push_back(0);
+        }
+        a= ans;
+        
+    }
+};
