2ea567a83f5701b49bc4c611a297a14fd0aa0a0f
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-18 01:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (43.04%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,15 +4,14 @@ public:
         vector<int> prefix(a.size(),1);
         vector<int> suffix(a.size(),1);
         for(int i=1;i<a.size();i++){
-            prefix[i] = a[i-1] * prefix[i-1];
+            prefix[i] = prefix[i-1]*a[i-1];
         }
-
         for(int i=a.size()-2;i>=0;i--){
             suffix[i] = suffix[i+1] * a[i+1];
         }
         for(int i=0;i<a.size();i++){
-            a[i] = prefix[i]*suffix[i];
+            prefix[i] = prefix[i] * suffix[i];
         }
-        return a;
+        return prefix;
     }
 };
