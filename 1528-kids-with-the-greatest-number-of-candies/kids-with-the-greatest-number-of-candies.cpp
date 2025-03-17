7d44346ce4438e6b1b33e2f528ad532e65e4994b
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-18 01:35:44 +0530

    Time: 4 ms (1.02%) | Memory: 12.4 MB (91.40%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& a, int k) {
+        vector<bool> ans(a.size(),true);
+        int greatest=0;
+        for(auto x: a){
+            if(x > greatest){
+                greatest = x;
+            }
+        }
+        for(int i=0;i<a.size();i++){
+            cout<<a[i]+k<<" ";
+            if((a[i]+k) >= greatest){
+                ans[i]=true;
+            }else{
+                ans[i]=false;
+            }
+        }
+        return ans;
+    }
+};
