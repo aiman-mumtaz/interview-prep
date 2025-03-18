3bec631b5cff3d8431f0fa68ebfe966b475425c9
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-19 03:28:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (94.35%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -5,17 +5,14 @@ public:
             swap(s1,s2); //s1 is always smaller
         }
         int len = gcd(s1.length(),s2.length());
-        cout<<len;
         string ans = "";
         for(int i=0;i<s1.size();i++){
             if(s1[i] != s2[i]){
-                cout<<"here";
                 return "";
             }
         }
         for(int i=0;i<len;i++){
             if(s1[i] != s2[i]){
-                cout<<"here";
                 return ans;
             }else{
                 ans+=s1[i];
@@ -25,7 +22,6 @@ public:
         cout<<s1[0];
         for(int i=len;i<s2.size();i++){
             if(j<s1.length() && s1[j] != s2[i]){
-                // cout<<s1[j]<<" "<<s2[i];
                 return "";
             }
             j++;
