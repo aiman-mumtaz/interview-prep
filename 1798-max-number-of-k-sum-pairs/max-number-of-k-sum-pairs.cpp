205222e146661bdcc1a1e66b53b9f021b259859f
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-20 05:04:23 +0530

    Time: 116 ms (27.31%) | Memory: 69.3 MB (38.79%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -2,28 +2,28 @@ class Solution {
 public:
     int maxOperations(vector<int>& a, int k) {
         int ans = 0;
-        sort(a.begin(),a.end());
-        int i=0,j=a.size()-1;
-        while(i<j){
-            if(a[i] + a[j] == k){
-                ans++;
-                i++;
-                j--;
-            }else if(a[i] + a[j] > k){
-                j--;
-            }else{
-                i++;
-            }
-        }
-        // vector<int> mpp;
-        // for(int i=0;i<a.size();i++){
-        //     if(find(mpp.begin(),mpp.end(),k-a[i]) != mpp.end()){
+        // sort(a.begin(),a.end());
+        // int i=0,j=a.size()-1;
+        // while(i<j){
+        //     if(a[i] + a[j] == k){
         //         ans++;
-        //         mpp.erase(find(mpp.begin(),mpp.end(),k-a[i]));
+        //         i++;
+        //         j--;
+        //     }else if(a[i] + a[j] > k){
+        //         j--;
         //     }else{
-        //         mpp.push_back(a[i]);
+        //         i++;
         //     }
         // }
+        unordered_map<int,int> mpp;
+        for(int i=0;i<a.size();i++){
+            if(mpp.find(k-a[i]) != mpp.end() && mpp[k-a[i]] > 0){
+                mpp[k-a[i]]--;
+                ans++;
+            }else{
+                mpp[a[i]]++;
+            }
+        }
         return ans;
     }
 };
