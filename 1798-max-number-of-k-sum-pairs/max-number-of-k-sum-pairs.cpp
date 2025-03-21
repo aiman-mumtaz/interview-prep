596f5360f55e7e89169a09f7850b8768830b95b2
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-22 02:40:15 +0530

    Time: 1936 ms (5.05%) | Memory: 64.3 MB (41.49%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,29 +1,47 @@
 class Solution {
 public:
-    int maxOperations(vector<int>& a, int k) {
-        int ans = 0;
-        sort(a.begin(),a.end());
-        int i=0,j=a.size()-1;
-        while(i<j){
-            if(a[i] + a[j] == k){
-                ans++;
-                i++;
-                j--;
-            }else if(a[i] + a[j] > k){
-                j--;
-            }else{
-                i++;
-            }
-        }
-        // unordered_map<int,int> mpp;
-        // for(int i=0;i<a.size();i++){
-        //     if(mpp.find(k-a[i]) != mpp.end() && mpp[k-a[i]] > 0){
-        //         mpp[k-a[i]]--;
+    int maxOperations(vector<int>& nums, int k) {
+        // int ans = 0;
+        // sort(a.begin(),a.end());
+        // int i=0,j=a.size()-1;
+        // while(i<j){
+        //     if(a[i] + a[j] == k){
         //         ans++;
+        //         i++;
+        //         j--;
+        //     }else if(a[i] + a[j] > k){
+        //         j--;
         //     }else{
-        //         mpp[a[i]]++;
+        //         i++;
         //     }
         // }
-        return ans;
+        // vector<int> mpp;
+        vector<pair<int, int>> freq;  // Vector of pairs: {number, frequency}
+        int operations = 0;
+
+        // Iterate through the numbers in the array
+        for (int num : nums) {
+            int complement = k - num;
+
+            // Binary search to find the complement in the vector
+            auto it = lower_bound(freq.begin(), freq.end(), make_pair(complement, 0));
+
+            if (it != freq.end() && it->first == complement && it->second > 0) {
+                // If complement is found in the vector and has a positive frequency, form a pair
+                operations++;
+                it->second--;  // Decrease the frequency of the complement
+            } else {
+                // Otherwise, insert the current number into the vector, maintaining sorted order
+                it = lower_bound(freq.begin(), freq.end(), make_pair(num, 0));
+                if (it != freq.end() && it->first == num) {
+                    // If the number is already in the vector, increase its frequency
+                    it->second++;
+                } else {
+                    // If the number is not in the vector, add it with frequency 1
+                    freq.insert(it, make_pair(num, 1));
+                }
+            }
+        }
+        return operations;
     }
 };
