6c1c4d974672f502d257f6e91bcc105b5a706a1e
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-25 02:47:13 +0530

    Time: 5 ms (57.96%) | Memory: 24 MB (35.39%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& a) {
+        sort(a.begin(),a.end());
+        vector<vector<int>> ans;
+        ans.push_back(a[0]);
+        for(int i=1;i<a.size();i++){
+            if(a[i][0] <= ans[ans.size()-1][1]){
+                if(a[i][1] > ans[ans.size()-1][1])
+                    ans[ans.size()-1][1]=a[i][1];
+            }else{
+                ans.push_back(a[i]);
+            }
+        }
+        return ans;
+    }
+};
