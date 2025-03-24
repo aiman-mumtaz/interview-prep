4dfc523321e4bede25702deb9423604c124a0965
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-25 02:48:02 +0530

    Time: 3 ms (89.75%) | Memory: 23.9 MB (35.39%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,8 +6,9 @@ public:
         ans.push_back(a[0]);
         for(int i=1;i<a.size();i++){
             if(a[i][0] <= ans[ans.size()-1][1]){
-                if(a[i][1] > ans[ans.size()-1][1])
+                if(a[i][1] > ans[ans.size()-1][1]){
                     ans[ans.size()-1][1]=a[i][1];
+                }
             }else{
                 ans.push_back(a[i]);
             }
