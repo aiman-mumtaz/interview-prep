7cd44a353abd5d492086e0bfd1b83f64dd1252fe
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-26 01:43:42 +0530

    Time: 29 ms (30.15%) | Memory: 82 MB (17.06%) - LeetSync

--- 816-design-hashset/design-hashset.cpp
+++ 816-design-hashset/design-hashset.cpp
@@ -1,64 +1,20 @@
-class Node {
-public:
-    int val;
-    Node* next;
-    Node(int val){
-        this->val = val;
-        this->next = NULL;
-    }
-};
 class MyHashSet {
 public:
-    Node* head;
+    bool arr[1000006] = {false};
     MyHashSet() {
-        head = NULL;
+        
     }
     
     void add(int key) {
-        if(head == NULL){
-            Node* newNode = new Node(key);
-            head = newNode;
-            return;
-        }
-        if(!contains(key)){        
-            Node* newNode = new Node(key);
-            Node* tmp = head;
-            while(tmp->next){
-                tmp = tmp->next;
-            }
-            tmp->next = newNode;
-        }
+        arr[key] = true;
     }
     
     void remove(int key) {
-        if(contains(key)){
-            if(head->val == key){
-                head = head->next;
-                return;
-            }
-            Node* tmp = head;
-            Node* prev = head;
-            while(tmp){
-                if(tmp->val == key){
-                    prev->next = tmp->next;
-                    tmp->next= NULL;
-                    return;
-                }
-                prev = tmp;
-                tmp = tmp->next;
-            }
-        }
+        arr[key] = false;
     }
     
     bool contains(int key) {
-        Node* tmp = head;
-        while(tmp){
-            if(tmp->val == key){
-                return true;
-            }
-            tmp = tmp->next;
-        }
-        return false;
+        return arr[key];
     }
 };
 
