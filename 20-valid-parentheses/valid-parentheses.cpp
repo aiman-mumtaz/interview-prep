720b91fb5aef37e51e4e9fdae40782056337a23d
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2025-03-30 02:18:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.79%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.length() == 1){
-            return false;
-        }
+        // if(s.length() == 1){
+        //     return false;
+        // }
         stack<char> st;
         for(auto x : s) {
             if(x == '(' || x == '[' || x == '{'){
